b7b7d9b232c1f80b2107619193f22d854db445ff
Debasu Eyasu <bayleyegn100@gmail.com>
2022-07-17 16:43:30 -0700

    Css for invoice included and invoice.js file imported to App.js.

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import Game from './Game.js';
 import Home from './Home.js';
 import TShirt from './TShirt.js';
 import Console from './Console.js';
+import Invoice from './Invoice.js';
 function App() {
   return (
     <main className="container">
-      <Game />
+      <div><Invoice /></div>
+      <div><Game /></div>
       <br /><br /><br />      
-      <Console />
+      <div><Console /></div>
       <br /><br /><br />
-      <TShirt />
+      <div><TShirt /></div>
        {/* <br /><br /><br />
        <br /><br /><br />
       <Home /> */}
