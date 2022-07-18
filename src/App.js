b79dd1f49339e7442be9cc1cc81dbff5a5f14bf8
Pat Chen <patchen21@gmail.com>
2022-07-18 11:45:46 -0400

    Revert "I think we need fronend for invoice. I have created invoiceForm, invoiceCard and invoice.js files"

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import Game from './components/Game/Game.js';
 import Home from './Home.js';
-import Invoice from './Invoice.js';
 import TShirt from './components/TShirt/TShirt.js';
 import Console from './components/Console/Console.js';
 import { BrowserRouter as Router, Route,Switch } from "react-router-dom";
@@ -14,10 +13,9 @@ function App() {
     <Route exact path="/game" component={Game} />
     <Route exact path="/console" component={Console} />
     <Route exact path="/tshirt" component={TShirt} />
-    <Route exact path="/invice" component={invoice} />
 </Switch>
 </Router>
-);
+  );
 }
 
 export default App;
